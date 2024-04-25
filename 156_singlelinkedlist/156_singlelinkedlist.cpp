fa6208d1fedd8d11dd43c9142880649db0bd1ed1
dannyfadli <danyfdly95@gmail.com>
2024-04-25 09:54:28 +0700

    meningkatkan pencarian  mencapai untuk node target lebih cepat, terutama dalam daftar yang lebih besar.

--- 156_singlelinkedlist/156_singlelinkedlist.cpp
+++ 156_singlelinkedlist/156_singlelinkedlist.cpp
@@ -55,6 +55,11 @@ bool serachNode(int nim, Node* current, Node* previous)
 {
     previous = START;
     current = START;
+    while (current != NULL && nim > current->noMhs)
+    {
+        previous = current;
+        current = current->next;
+    }
 }
 
 int main()
