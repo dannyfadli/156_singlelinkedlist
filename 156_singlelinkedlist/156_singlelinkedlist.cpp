e4c2468c180431a9b2ac892ba7150be96a09d400
dannyfadli <danyfdly95@gmail.com>
2024-04-25 10:15:59 +0700

    "Diimplementasikan fungsi traverse dasar untuk traversal daftar dan penanganan daftar kosong"

--- 156_singlelinkedlist/156_singlelinkedlist.cpp
+++ 156_singlelinkedlist/156_singlelinkedlist.cpp
@@ -95,6 +95,16 @@ bool listEmpty()
         return false;
 }
 
+void traverse()
+{
+    if (listEmpty())
+    {
+        cout << "List kosong" << endl;
+        system("pause");
+        system("cls");
+    }
+}
+
 int main()
 {
     
