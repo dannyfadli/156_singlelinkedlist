1c67e82866c4035e0a8d971b46423af7369e5cad
dannyfadli <danyfdly95@gmail.com>
2024-04-25 09:33:32 +0700

    menambahkan addNode  yang akan menambahkan node baru ke daftara dengan mimanta nim dan nama

--- 156_singlelinkedlist/156_singlelinkedlist.cpp
+++ 156_singlelinkedlist/156_singlelinkedlist.cpp
@@ -8,6 +8,22 @@ struct Node
     Node* next;
 };
 
+Node* START = NULL;
+
+void addNode()
+{
+    int nim;
+    string nama;
+    Node* nodeBaru = new Node();
+    cout << "Masukan Nim: ";
+    cin >> nim;
+    cout << "Masukan Nama: ";
+    cin >> nama;
+    nodeBaru->noMhs = nim;
+    nodeBaru->name = nama;
+
+}
+
 int main()
 {
     
